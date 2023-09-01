b693aac93cd88526f2f14415cad0e436e80dbceb
Gang <jerry050512@outlook.com>
2023-09-01 22:04:35 +0800

    Add comments to the core code

--- log.py
+++ log.py
@@ -2,24 +2,31 @@ from window_terminal import WindowTerminal
 from datetime import datetime
 
 class LogWindow:
+    '''A window terminal that shows the log info and save log files.'''
+
     def __init__(self, logfile='log.log'):
         self.window = WindowTerminal.create_window()
         self.logfile = logfile
         self._isopen = False
 
     def open_window(self):
+        '''Open the LogWindow.'''
         self.window.open()
         self.window.print('Log Window opened...')
         self.window.print(f'Log file will be written to {self.logfile}...')
         self._isopen = True
 
     def wait_for_enter(self):
+        '''Wait until user press enter key.'''
         if not self._isopen:
             self.log('')
             return 
         self.window.input('Enter to continue...')
     
     def log(self, message: str, type='log'):
+        '''Print the log message on the log window and save the message to the logfile. 
+        When the type is specified as "error" or part of it, the print message will be 
+        red in foreground and white in background.'''
         now = datetime.now()
         if not self._isopen:
             print('Log window not open!')
@@ -30,6 +37,7 @@ class LogWindow:
         
         self.window.print(now.strftime('\n%Y-%m-%d %H:%M:%S'))
 
+        # \033[47m is red foreground and \033[31m is white background.
         if type.lower() in 'error':
             self.window.print('\033[47m\033[31m', end='')
         self.window.print(f'{type.upper()}: {message}\033[0m')
@@ -40,4 +48,5 @@ class LogWindow:
                 f.write('[ERROR] ')
             f.write(f'{message}\n\n')
 
+# This is an instance of LogWindow that can be use directly.
 log_wd = LogWindow()
--- song_lyrics_spider.py
+++ song_lyrics_spider.py
@@ -9,6 +9,7 @@ import json
 
 from log import log_wd
 
+# The album page of a singer in the musixmatch.com
 TARGET_URL = 'https://www.musixmatch.com/artist/%E4%BA%94%E6%9C%88%E5%A4%A9/albums'
 MAX_ATTEMPT_TIME = 50
 JSON_INDENT = 4
@@ -18,6 +19,10 @@ get_resource_name = lambda url: unquote(url.split('/')[-1])
 pause = lambda: system('pause')
 
 def recaptcha_detect(driver: webdriver.Edge):
+    '''Detect if the page was redirected to the recaptcha. When the site was accessed 
+    too many times, it will be redirected to the recaptcha page.
+    
+    Return: 1 if redirected else 0'''
     current_url = driver.current_url
     if 'verify-user' in current_url: 
         log_wd.log('Wait for the recaptcha...')
@@ -27,6 +32,10 @@ def recaptcha_detect(driver: webdriver.Edge):
     return 0
 
 def load_page(driver: webdriver.Edge):
+    '''Try to click the load more button many times so that the webpage can
+    be fully loaded.
+    
+    Return: None'''
     driver.refresh()
     i, max_time = 1, 6
     while i < max_time:
@@ -37,6 +46,7 @@ def load_page(driver: webdriver.Edge):
             driver.execute_script("arguments[0].scrollIntoView();", button)
             button.click()
         except:
+            # Anything wrong? Just wait, wait, and wait...
             log_wd.log(f'Unable to click the load button, wait {i+2} secs...', 'error')
             driver.refresh()
             max_time += 1
@@ -53,6 +63,9 @@ def load_page(driver: webdriver.Edge):
     log_wd.log('Root page loaded...')
 
 def get_albums_url(driver: webdriver.Edge):
+    '''Get albums url from current page.
+    
+    Return: list of albums url'''
     try:
         albums = driver.find_elements(By.CSS_SELECTOR, "div > h2.media-card-title > a")
         albums = [album.get_attribute('href') for album in albums]
@@ -64,6 +77,9 @@ def get_albums_url(driver: webdriver.Edge):
     return albums
 
 def write_title(url: str, type: str):
+    '''Just a function to quick write the title or album name of the song...
+    
+    Return: title(str) of the resource'''
     title = get_resource_name(url)
     line_break = '\n' * 3 if title == 'Album' else '\n' * 2
     with open('lyrics.txt', 'a+', encoding='utf-8') as f:
@@ -71,6 +87,9 @@ def write_title(url: str, type: str):
     return title
 
 def get_songs_from_album(driver: webdriver.Edge, url: str):
+    '''Get the songs\' url in the given album url.
+    
+    Return: list of songs\' url'''
     driver.get(url)
     while recaptcha_detect(driver):
         driver.get(url)
@@ -84,10 +103,14 @@ def get_songs_from_album(driver: webdriver.Edge, url: str):
     return songs
 
 def get_lyrics_from_song(driver: webdriver.Edge, url: str, accessed_songs=None):
+    '''Get the lyrics from the song page. Skip songs in accessed_songs(list).
+    
+    Return: lyrics(str)'''
     driver.get(url)
     while recaptcha_detect(driver):
         driver.get(url)
     
+    # Check if accessed
     if accessed_songs is None:
         accessed_songs = []
     song_name = get_resource_name(url)
@@ -98,6 +121,7 @@ def get_lyrics_from_song(driver: webdriver.Edge, url: str, accessed_songs=None):
     sleep(SLEEP_TIME)
     
     try:
+        # If the page do not have lyrics due to some reasons, it renders this.
         empty_title = driver.find_element(By.CSS_SELECTOR, 'h2.mxm-empty__title').text
         log_wd.log(f'{song_name}: {empty_title}', 'error')
     except:
@@ -109,6 +133,7 @@ def get_lyrics_from_song(driver: webdriver.Edge, url: str, accessed_songs=None):
         pass
 
     try:
+        # When the lyrics have some error to modify, it renders this.
         lyrics = [lyric.text for lyric in driver.find_elements(By.CSS_SELECTOR, "span.lyrics__content__error")]
         if lyrics:
             lyrics = "\n\n".join(lyrics) + '\n\n\n'
@@ -118,8 +143,8 @@ def get_lyrics_from_song(driver: webdriver.Edge, url: str, accessed_songs=None):
         pass
 
     try:
+        # Normally the lyrics render like this.
         lyrics = [lyric.text for lyric in driver.find_elements(By.CSS_SELECTOR, "span.lyrics__content__ok")]
-        # lyrics = [lyric.text for lyric in driver.find_elements(By.CSS_SELECTOR, "span.lyrics__content__error")]
         if lyrics:
             lyrics = "\n\n".join(lyrics) + '\n\n\n'
             log_wd.log(f'Get lyrics of {song_name}')
@@ -127,11 +152,18 @@ def get_lyrics_from_song(driver: webdriver.Edge, url: str, accessed_songs=None):
             log_wd.log(f'Unable to get lyrics of {song_name}', 'error')
             lyrics = ""
     except:
+        # I don't know how to cause this exception, but it truly happened.
         log_wd.log(f'Unable to get lyrics of {song_name}', 'error')
         lyrics = ""
     return lyrics
 
 def exit_handler(accessed_songs: list):
+    '''Save file before exit.
+
+    Note. Changes will not be saved if you click the close button. But this can 
+    handle the Ctrl+C keyborad interruption.
+    
+    Return: None'''
     log_wd.log('Exit program. Saving data...')
     print('Exit program. Saving data...')
     with open('saved_songs.json', 'w', encoding='utf-8') as f:
@@ -139,12 +171,14 @@ def exit_handler(accessed_songs: list):
 
 
 def main():
+    # Open the log window.
     log_wd.open_window()
 
     options = Options()
     driver = webdriver.Edge()
     driver.get(TARGET_URL)
 
+    # Read the accessed_song list.
     accessed_songs = []
     try:
         with open('saved_songs.json', 'r', encoding='utf-8') as f:
@@ -152,22 +186,27 @@ def main():
     except:
         log_wd.log('saved_song.json did not exist or invalid expression in the file...', 'error')
 
+    # Register the exit_handler.
     atexit.register(exit_handler, accessed_songs)
 
+    # Load the root album page.
     while recaptcha_detect(driver):
         driver.get(TARGET_URL)
     load_page(driver)
 
     albums = get_albums_url(driver)
     driver.quit()
+    # Switch the option to save loading time.
     options.page_load_strategy = 'none'
     driver = webdriver.Edge(options=options)
 
 
+    # Iterate the albums list
     for album in albums:
         songs = get_songs_from_album(driver, album)
         album_name = get_resource_name(album)
         log_wd.log(f'Iterate in album {album_name}')
+        # Use the flag to confirm if the album has something to save.
         flag = False
         for song in songs:
             lyrics = get_lyrics_from_song(driver, song, accessed_songs)
